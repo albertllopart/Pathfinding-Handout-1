ef68d4739f3b04905017637abefd0305519e8c8e
Albert Llopart Navarra <d.pocafeiner@gmail.com>
2017-10-18 09:54:57 +0200

    TODO 1 començat

--- Dev_class7.5_handout/Motor2D/j1Map.cpp
+++ Dev_class7.5_handout/Motor2D/j1Map.cpp
@@ -39,6 +39,10 @@ void j1Map::PropagateBFS()
 {
 	// TODO 1: If frontier queue contains elements
 	// pop the last one and calculate its 4 neighbors
+	if (frontier.Count() > 0)
+	{
+		p2Queue_item<iPoint>* item = frontier.GetLast();
+	}
 
 	// TODO 2: For each neighbor, if not visited, add it
 	// to the frontier queue and visited list
